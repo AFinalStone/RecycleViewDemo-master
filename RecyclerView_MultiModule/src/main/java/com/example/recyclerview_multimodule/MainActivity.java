665fe02419497a63c9317471c5420d51983f45b5
AFinalStone <602392033@qq.com>
2017-10-27 19:32:15 +0800

    修改项目Model名称

--- RecyclerView_MultiModule/src/main/java/com/example/recyclerview_multimodule/MainActivity.java
+++ RecyclerView_MultiModule/src/main/java/com/example/recyclerview_multimodule/MainActivity.java
@@ -2,12 +2,32 @@ package com.example.recyclerview_multimodule;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.example.adapter.GeneralAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private List<String> listData;
+    private GeneralAdapter generalAdapter;
+    private RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mRecyclerView = (RecyclerView) findViewById(R.id.mRecyclerView);
+        listData = new ArrayList<String>();
+        for (int i = 'A'; i < 'z'; i++) {
+            listData.add("" + (char) i);
+        }
+        generalAdapter = new GeneralAdapter(this, listData);
+        //设置GridLayoutManager布局管理器，实现GridLayoutManager效果
+        mRecyclerView.setLayoutManager(new GridLayoutManager(this, 6, GridLayoutManager.VERTICAL, false));
+        mRecyclerView.setAdapter(generalAdapter);
     }
 }
